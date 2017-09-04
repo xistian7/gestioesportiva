ed78a4fff7cf420059d5a8d2172caf7c2bdf9cc6
Xistian7 <cris.batea@gmail.com>
2017-09-04 23:25:10 +0200

    ja funciona

--- backend/config/main.php
+++ backend/config/main.php
@@ -1,9 +1,9 @@
 <?php
 $params = array_merge(
     require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+    //require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php')
+    //require(__DIR__ . '/params-local.php')
 );
 
 return [
@@ -15,6 +15,7 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
+            'cookieValidationKey' => 'DoingITeasy',
         ],
         'user' => [
             'identityClass' => 'common\models\User',
--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,21 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=mysql514int.srv-hostalia.com;dbname=db5210912_gesport',
+            'username' => 'u5210912_gesport',
+            'password' => '6BIC8rrm',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+   
     ],
 ];
